52baea56478eef54d90320e532c73a282c10bce6
yashiroryo <>
2023-11-18 16:34:39 +0900

    feat: typescript化

---
+++ backend/index.ts
@@ -0,0 +1,107 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    tasks: [Task]
+  }
+
+  type Mutation {
+    createTask(body: String!): Task
+    updateTaskBody(id: String!, body: String!): Task
+    updateCompleteState(id: String!, isComplete: Boolean!): Task
+    deleteTask(id: String!): Task
+  }
+
+  type Task {
+    id: ID!
+    body: String!
+    isComplete: Boolean!
+    createdAt: String!
+  }
+`;
+
+/* ダミーデータ */
+let lastTaskIndex = 2;
+const tasks = [
+  {
+    id: "1",
+    body: "ご飯作る",
+    isComplete: false,
+    createdAt: "2023:09:15 22:33:00",
+  },
+  {
+    id: "2",
+    body: "宿題する",
+    isComplete: false,
+    createdAt: "2023:09:15 23:45:00",
+  },
+];
+
+const resolvers = {
+  Query: {
+    tasks: () => tasks,
+  },
+  Mutation: {
+    createTask: (_: any, args: { body: string }) => {
+      const newTask = {
+        id: String(lastTaskIndex + 1),
+        body: args.body,
+        isComplete: false,
+        createdAt: String(new Date().getTime()),
+      };
+      lastTaskIndex++;
+      tasks.push(newTask);
+      return newTask;
+    },
+    updateTaskBody: (_: any, args: { id: string; body: string }) => {
+      const index = tasks.findIndex((task) => task.id === args.id);
+      if (index === -1) {
+        return null;
+      }
+      const newTask = {
+        id: tasks[index].id,
+        body: args.body,
+        isComplete: tasks[index].isComplete,
+        createdAt: String(new Date().getTime()),
+      };
+      tasks.splice(index, 1, newTask);
+      return newTask;
+    },
+    updateCompleteState: (
+      _: any,
+      args: { id: string; isComplete: boolean }
+    ) => {
+      const index = tasks.findIndex((task) => task.id === args.id);
+      if (index === -1) {
+        return null;
+      }
+      const newTask = {
+        id: tasks[index].id,
+        body: tasks[index].body,
+        isComplete: args.isComplete,
+        createdAt: String(new Date().getTime()),
+      };
+      tasks.splice(index, 1, newTask);
+      return newTask;
+    },
+    deleteTask: (_: any, args: { id: string }) => {
+      const index = tasks.findIndex((task) => task.id === args.id);
+      if (index === -1) {
+        return null;
+      }
+      const deleteTargetTask = tasks[index];
+      tasks.splice(index, 1);
+      return deleteTargetTask;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req, res }) => ({ req, res }),
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
